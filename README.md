27de3d9c4a14a53542d87944eb4031e796ebbf65
Marc Lucke <marc@marcsnet.com>
2013-10-26 13:41:17 +1100

    - moved to sqlite for performance reasons - writing out an entire dictionary every imap folder didn't make a lot of sense. - cosmetic changes, status bars etc.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 imap_autosort_nilsimsa
 ======================
 
-use nilsimsa on email headers to automatically sort your inbox into imap folders
+This is a pre-alpha script - more of a concept if you like.  It actually works, but it is undergoing sporadic development.
+
+
+The purpose is to use nilsimsa on email headers to automatically sort your inbox into imap folders.  It is built on Python 3 but wil work in Python 2 with minimal modification.
+
+Use the imap_nilsima.conf for configuration; a example is provided: imap_nilmsa.conf.sample
+
+For the IMAP folders you specify the script will build a database of nilsimsa hex digests of the headers of all of the email in that folder.  When sorting, the script will look for unread email in your inbox, calculate the nilsimsa hexdigest, calculate the nilsimsa distance and use that to score where the email is moved to.
+
+Nilsimsa provides a distance from -127 (2 bits of text are nothing alike) to 128 (2 bits of text are the same).  Through analysis of my email messages I find that the headers of like email score ~80 which I call the threshold.  If an email scores below 80 is is not scored.  The score is caluclated such that it is out of 100 per email on a factor of 2 (parabolic).  Those scores are then added together per folder.  Whichever folder has the highest score gets the mail.  This algorithm may be subject to tuning but thus far it works quite well.  The closer the email is to an email that exists in the target IMAP folder is, the higher it scores BUT if there are many more lower scoring emails in another folder then the score can be overriden. 
+
+You don't need to understand the script to use it and you may have varying levels of success with it.  I encourage feedback to marc@marcsnet.com
--- imap_nilsimsa.py
+++ imap_nilsimsa.py
@@ -1,4 +1,4 @@
-import imaplib, pickle, sys, email, time, configparser
+import imaplib, pickle, sys, email, time, configparser, sqlite3
 from nilsimsa import *
 from optparse import OptionParser
 
@@ -7,20 +7,55 @@ config.read('imap_autosort.conf')
 
 imap_folders = [x.strip() for x in config['imap']['folders'].split(',')]
 threshold = float(config['nilsimsa']['match_threshold'])
+sqlite3_db = '.imap_nilsimsa.db'
+twirly = ('|','/','-','\\')
+
+def status(num,max,message=''):
+    if not max: return
+    percent = int(100*int(num)/int(max)+1)
+    num_equals=int(percent/2)
+    sys.stdout.write("%s [%-50s] %3d%%\r" % (message,'=' * num_equals,percent))
+    sys.stdout.flush()
 
 def load_hexdigest(folder,debug=False):
-    if folder not in digests.keys(): digests[ folder ] = {}
     mail.select( '"'+folder+'"', readonly = True ) # connect to inbox.
+    
+    # get email_uids for this folder
     result, data = mail.uid('search', None, "ALL")
     email_uids = data[0].decode().split()
-    deleted_uids = []
-    for digest_uid in digests[ folder ].keys():
-        if digest_uid not in email_uids: deleted_uids.append( digest_uid )
-    for deleted_uid in deleted_uids:
-            print ("Removing digest of deleted message {} in folder {}".format(deleted_uid,folder))
-            del(digests[ folder ][ deleted_uid ])
-    for email_uid in email_uids:
-        if email_uid in digests[ folder ].keys(): continue
+
+    # if an email has been deleted or moved from the imap folder, remove it from the db
+    del_cursor = db_connect.cursor()
+    del_cursor.execute('select id,uid from nilsimsa where folder=?',[folder])
+    count = 0
+    for row in del_cursor:
+        # this is pretty annoying - fixme
+        sys.stdout.write("Deletion anaylsis %s count=%6d\r" % (twirly[count % 4],count))
+        sys.stdout.flush()
+        count += 1
+        
+        if str(row[1]) not in email_uids:
+            if debug:
+                print ("removing uid: {} from folder: {} id: {}".format(row[1],folder,row[0]))
+            cursor.execute('delete from nilsimsa where id=?',[row[0]])
+    print()
+
+    uid_count = len(email_uids)
+    for i in range(uid_count):
+        status(i,uid_count,'examining')
+        continue
+        email_uid = email_uids[i]
+        # if we've seen this email_uid already, continue
+        cursor.execute('select count(*) from nilsimsa where folder=? and uid=?',[folder,email_uid])
+        row = cursor.fetchone()
+        if row[0] != 0:
+            if debug:
+                print ("uid: {} seen - skipping".format(email_uid))
+            continue
+        else:
+            print ("processing uid: {}".format(email_uid))
+        
+        # process header
         result, data = mail.uid('fetch', email_uid, '(BODY.PEEK[HEADER])')
         raw_header = data[0][1].decode('latin-1')
         try:
@@ -30,9 +65,8 @@ def load_hexdigest(folder,debug=False):
         hexdigest = nilsimsa.hexdigest()
         if debug:
             print ("{}: {}: {}".format(folder, email_uid, hexdigest))
-        digests[ folder ][ email_uid ] = hexdigest
-    with open('nilsima_hexdigests.pickle', 'wb') as f:
-        pickle.dump(digests, f, pickle.HIGHEST_PROTOCOL)
+        cursor.execute('insert into nilsimsa (uid,folder,hexdigest) values(?,?,?)',[email_uid,folder,hexdigest])
+    db_connect.commit()
 
 def autosort_inbox(folders,debug=False):
     mail.select('inbox', readonly = False)
@@ -43,26 +77,39 @@ def autosort_inbox(folders,debug=False):
         result, data = mail.uid('fetch', email_uid, '(BODY.PEEK[HEADER])')
         raw_header = data[0][1].decode('latin-1')
         msg = email.message_from_string( raw_header )
-        print("  Source: subject: {}".format(msg['Subject']))
-        print("  Source: from: {}".format(msg['From']))
-        print("  Source: to: {}".format(msg['To']))
+        try:
+            print("  Source: subject: {}".format(msg['Subject']))
+        except:
+            print( "Can't print the subject.  Python 2?" )
+            pass
+        try:
+            print("  Source: from: {}".format(msg['From']))
+        except:
+            print( "Can't print the subject.  Python 2?" )
+            pass
+        try:
+            print("  Source: to: {}".format(msg['To']))
+        except:
+            print( "Can't print the subject.  Python 2?" )
+            pass
         try:
             nilsimsa = Nilsimsa( raw_header )
         except:
             continue
         source_hexdigest = nilsimsa.hexdigest()
         
-        # get averages of all scores over 80
+        # get averages of all scores over threshold
         scores = {}
         for folder in folders: scores[ folder ] = 0.0
         for folder in folders:
             sum = 0.0
             count = 0.0
-            for target_hexdigest in digests[ folder ].values():
-                distance = compare_hexdigests(source_hexdigest, target_hexdigest)
+            cursor.execute('select hexdigest from nilsimsa where folder=?',[folder])
+            for row in cursor:
+                distance = compare_hexdigests(source_hexdigest, row[0])
                 if distance > threshold:
                     if debug:
-                        print ('folder: {} match {} is over 80'.format(folder,distance))
+                        print ('folder: {} match {} is over {}'.format(folder,distance,threshold))
                     # the score should always be out of 100
                     # I want to weight higher matches parabolically
                     score = ( 10 * (distance - threshold) / ( 128 - threshold )) ** 2 
@@ -72,13 +119,25 @@ def autosort_inbox(folders,debug=False):
                     # for debugging
                     if debug:
                         msg = email.message_from_string( raw_header )
-                        print("  Source: subject: {}".format(msg['Subject']))
-                        print("  Source: from: {}".format(msg['From']))
-                        print("  Source: to: {}".format(msg['To']))
+                        try:
+                            print("  Source: subject: {}".format(msg['Subject']))
+                        except:
+                            print( "Can't print the subject.  Python 2?" )
+                            pass
+                        try:
+                            print("  Source: from: {}".format(msg['From']))
+                        except:
+                            print( "Can't print the subject.  Python 2?" )
+                            pass
+                        try:
+                            print("  Source: to: {}".format(msg['To']))
+                        except:
+                            print( "Can't print the subject.  Python 2?" )
+                            pass
             if count > 0:
                     scores [ folder ] = sum
                     average = sum / count
-                    print ('{} messages scored over 80 in folder {} - sum = {}, average = {}'.format(count,folder,sum,average))
+                    print ('{} messages scored over {} in folder {} - sum = {}, average = {}'.format(count,threshold,folder,sum,average))
             
         # which folder is the winner?
         winner = max(scores, key=scores.get)
@@ -96,39 +155,43 @@ def autosort_inbox(folders,debug=False):
                     
 
 if __name__ == "__main__":
-    # if we've done this before, load in the data
-    try: 
-        with open('nilsima_hexdigests.pickle', 'rb') as f:
-            digests = pickle.load(f)
-    except: 
-        digests={}
+    # connect to sqlitedb, create table/s if necessary
+    db_connect=sqlite3.connect(sqlite3_db)
+    cursor=db_connect.cursor()
+    cursor.execute('create table if not exists nilsimsa (id INTEGER PRIMARY KEY AUTOINCREMENT , uid INTEGER, folder TEXT, hexdigest TEXT)')
     
     parser = OptionParser(usage="python imap_nilsimsa.py or --help", description=__doc__)
-    parser.add_option("-r", "--refresh", action="store_true", dest="refresh", default=False, help="Refresh (or build) the imap nilsimsa hex digests")
+    parser.add_option("-r", "--rebuild", action="store_true", default=False, dest="rebuild", help="Rebuild (or build) the imap nilsimsa hex digests.  When used in combination with -l, will rebuild every -r loops")
     parser.add_option("-s", "--autosort", action="store_true", dest="sort", default=False, help="Sort email in the inbox")
     parser.add_option("-d", "--debug", action="store_true", default=False, dest="debug", help="debug information")
     parser.add_option("-l", "--loop", action="store", type="float", default=0.0, dest="loop", help="loop -l seconds")
+    parser.add_option("--rebuild_every", action="store", type="float", default=10.0, dest="rebuild_every", help="rebuild every rebuild_every loop count")
     (options, args)= parser.parse_args()
 
-    do_loop = True
-    while do_loop:
-        print ("Processing: {}".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))      
+    loop_count = 0.0
+    while True:
+        print ("Processing: {}".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+    
         # log in to the server
         mail = imaplib.IMAP4_SSL( config['imap']['server'] )
         mail.login( config['imap']['username'], config['imap']['password'] )
         
-        if options.refresh:
+        if options.rebuild and not loop_count % options.rebuild_every:
             for imap_folder in imap_folders:
-                print ("Analysing " + imap_folder )
+                print ("\nAnalysing " + imap_folder )
                 load_hexdigest( imap_folder, options.debug )
         if options.sort:
             autosort_inbox( imap_folders, options.debug )
-        
-        mail.close()
-        mail.logout()
-        
+               
         if options.loop > 0:
             print ("Sleeping for {} seconds".format(options.loop))
             time.sleep(options.loop)
         else:
-            do_loop = False
+            break
+            
+        db_connect.close()
+        mail.close()
+        mail.logout()
+            
+        loop_count += 1
+ 
